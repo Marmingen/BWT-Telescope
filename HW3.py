4e1b972a0ca8c575847cda7c1c93ea205dfe1a40
Marmingen <melker.marminge.1030@student.uu.se>
2023-05-19 14:29:52 +0200

    updates

--- HW3.py
+++ HW3.py
@@ -5,7 +5,11 @@
 
 import math
 
+#####################################################################
+## TELESCOPE CLASS
+
 class Telescope():
+    """initialized with UU-BWT information"""
     def __init__(self, theta_blaze=66.5, f_col=0.8, L_grate=0.2, 
                  gpm=72e3, diameter=0.9, CCD_pixelsize=15e-6,
                  focal_ratio=0.25):
@@ -23,7 +27,7 @@ class Telescope():
         #### calculated data ####
         self.N = L_grate*gpm # amount of grooves
         self.delta = 1/gpm # m
-        self.W = L_grate**2 # assuming square grating
+        self.W = L_grate # m
     
     def set_seeing(self, seeing):
         """input seeing as arcsec"""
@@ -48,24 +52,29 @@ class Telescope():
     def print_wavelength_data(self):
         if self.image and self.target_image:
             print("data for the wavelengths")
-            print("|","-"*62, "|")
-            print(f"| {'wavelength':<10} | {'optimal m':<10} | {'optimal s':<10} | {'f_cam':<10} | {'true res':<10} |")
+            print("|","-"*85, "|")
+            print(f"| {'wavelength [Å]':<15} | {'optimal m':<10} | {'optimal s [microns]':<20} | {'f_cam [m]':<14} | {'true res':<14} |")
             for wave in self.wavelengths:
-                print(f"| {wave.wavelength:<10.3e} | {wave.m:<10} | {wave.optimal_s:<10.3e} | {wave.f_cam:<10.3e} | {wave.true_resolution:<10.2e} |")
-            print("|", "-"*62, "|")
+                print(f"| {wave.wavelength/1e-10:<15.3f} | {wave.m:<10} | {wave.optimal_s/1e-6:<20.3f} | {wave.f_cam:<14.3f} | {wave.true_resolution:<14.2f} |")
+            print("|", "-"*85, "|")
         else:
             print("set local seeing and desired sampling rate first")
             
     def print_telescope_data(self):
         print("data for the telescope")
-        print("|", "-"*52, "|")
-        print(f"| {'diameter':<10} | {'f_tel':<10} | {'f_col':<10} | {'blaze angle':<14}|")
-        print(f"| {self.diameter:<10} | {self.diameter/self.focal_ratio:<10.2f} | {self.f_col:<10} | {math.degrees(self.theta_blaze):<14.2f}|")
-        print("|", "-"*52, "|")
-        print(f"| {'L_grate':<10} | {'gpm':<10} | {'pixelsize':<10} | {'N':<14}|")
-        print(f"| {self.L_grate:<10} | {self.gpm:<10} | {self.CCD_pixelsize:<10.2e} | {self.N:<14}|")
-        print("|", "-"*52, "|")
+        print("|", "-"*85, "|")
+        print(f"| {'diameter [m]':<20} | {'f_tel [m]':<20} | {'f_col [m]':<20} | {'blaze angle [deg]':<17}|")
+        print(f"| {self.diameter:<20} | {self.diameter/self.focal_ratio:<20.2f} | {self.f_col:<20} | {math.degrees(self.theta_blaze):<17.2f}|")
+        print("|", "-"*85, "|")
+        print(f"| {'L_grate [m]':<20} | {'gpm [1/m]':<20} | {'pixelsize [microns]':<20} | {'N':<17}|")
+        print(f"| {self.L_grate:<20} | {self.gpm:<20} | {self.CCD_pixelsize/1e-6:<20.2f} | {self.N:<17}|")
+        print("|", "-"*85, "|")
+        
+#####################################################################
+## WAVELENGTH CLASS
+
 class Wavelength():
+    
     def __init__(self, wavelength, telescope):
         
         #### input data ####
@@ -107,18 +116,17 @@ def main():
     lam2 = 6000e-10
     lam3 = 8000e-10
     lam4 = 256000e-10
+    lam5 = 1e-7
     
     lambs = [lam1, lam2, lam3]
     
     BWT = Telescope()
     
-    # BWT.diameter = 44
-    
+    BWT.diameter = 39
+
     BWT.set_seeing(2)
     BWT.set_sampling(3)
     
-    
-    # print(BWT.image)
     
     BWT.add_wavelengths(lambs)
     
@@ -128,9 +136,9 @@ def main():
     BWT.print_wavelength_data()
     
     BWT.print_telescope_data()
-    # for wave in BWT.wavelengths:
-    #     print(wave.wavelength, "|", wave.true_resolution)
-    #     print(wave.optimal_s/BWT.f_col)
+
+#####################################################################
+## RUN CODE
 
 if __name__ == "__main__":
     main()
